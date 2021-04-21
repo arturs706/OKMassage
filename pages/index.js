f61e3f489214fe02612ff5fdbcb2b4d01d5ee5b2
arturs706 <45068546+arturs706@users.noreply.github.com>
2021-04-21 21:23:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
     <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </Head>
       <Container1/>
       <Container2/>
