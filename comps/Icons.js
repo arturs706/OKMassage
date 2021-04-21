9f1c425985d8a60409c34ae2e962cbdd77faefe3
arturs706 <45068546+arturs706@users.noreply.github.com>
2021-04-22 00:20:37 +0100

    Update Icons.js

--- comps/Icons.js
+++ comps/Icons.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { IconContext } from "react-icons";
 import { RiFacebookLine, RiInstagramLine, RiLinkedinLine, RiMessengerLine, RiTelegramLine, RiWhatsappLine} from "react-icons/ri";
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 const IconMenu = styled.div`
 display: flex;
@@ -23,13 +24,18 @@ max-width: 100rem;
 function Icons(props) {
     return (
         <IconMenu>
+
             <IconContext.Provider value={{className: "icon-class-name" }}>
+            <Fade bottom distance = "300px" delay = {3000} duration = {3000} ssrFadeout = {true}>
+
             <a href="https://m.facebook.com/Okmassage-107617684179566/"><RiFacebookLine/></a>
             <a href="https://www.instagram.com/ksjukis/"><RiInstagramLine/></a>
             <a href="https://www.linkedin.com/"><RiLinkedinLine/></a>
             <a href="https://www.linkedin.com/"><RiMessengerLine/></a>
             <a href="https://www.linkedin.com/"><RiTelegramLine/></a>
             <a href="https://wa.link/5j5c92"><RiWhatsappLine/></a>
+            </Fade>
+
             </IconContext.Provider>
         </IconMenu>
     )
