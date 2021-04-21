5311216719e8c9279d0df32c229c1fd86b72f89d
arturs706 <45068546+arturs706@users.noreply.github.com>
2021-04-21 21:22:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ import FormContainer from '../comps/FormContainer'
 export default function Home() {
   return (
     <div>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </Head>
       <Container1/>
       <Container2/>
       <Container3/>
