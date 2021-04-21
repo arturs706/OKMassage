07a4b132210a3dd9954d8010445d703c5a65fc08
arturs706 <45068546+arturs706@users.noreply.github.com>
2021-04-21 21:50:43 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
 import Layout from '../comps/Layout'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return (
+  return (   
           <Layout>
+             <Head>
+            <meta charset="UTF-8"/>
+              <meta name="description" content="Free Web tutorials"/>
+              <meta name="keywords" content="Massage in Barnet, Massage in London, Spa, Day off "/>
+              <meta name="author" content="Arthur"/>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          </Head>
             <Component {...pageProps} />
           </Layout>)
 }
+    
 
 export default MyApp
